680d8494317568a6d32c856b246f6542b303a482
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2023-06-16 10:26:50 +0700

    6 Create entity, repo (User/security)

--- src/main/java/com/hotelBooking/hotelbooking/user/Repository/UserRepository.java
+++ src/main/java/com/hotelBooking/hotelbooking/user/Repository/UserRepository.java
@@ -2,9 +2,9 @@ package com.hotelBooking.hotelbooking.user.Repository;
 
 import com.hotelBooking.hotelbooking.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
--- src/main/java/com/hotelBooking/hotelbooking/user/entity/Role.java
+++ src/main/java/com/hotelBooking/hotelbooking/user/entity/Role.java
@@ -1,6 +1,15 @@
 package com.hotelBooking.hotelbooking.user.entity;
 
-public enum Role {
-    USER,
-    ADMIN
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
 }
+
